5e24da2cb3d9948fb0370851777077cbe6e1adb8
swangechi <sgituire@gmail.com>
2024-06-05 17:00:24 +0300

    First commit

---
+++ jsSample.js
@@ -0,0 +1,4 @@
+console.log("----------")
+console.log("A new project!")
+console.log("All by myself")
+console.log("----------")
---
+++ samplePython.py
@@ -0,0 +1,4 @@
+print("--------------------")
+print("Python entered chat")
+print("Yaaay! python is here")
+print("-------------------")
